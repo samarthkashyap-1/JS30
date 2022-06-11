56c82c696e9ec6915447add3575d2e3a26ac3285
samarthkashyap-1 <93036927+samarthkashyap-1@users.noreply.github.com>
2022-06-11 14:57:10 +0530

    Bug fixed
    Input Bug fixed
    Scoreboard bug fixed
    Font Bug fixed

--- JS GTS/app.js
+++ JS GTS/app.js
@@ -3,31 +3,25 @@ let cityState = "";
 let count = 0;
 const cities = [];
 const input = document.querySelector("#input");
- 
+
 const start = document.querySelector(".play");
 const inp = document.querySelector(".input");
 const score = document.querySelector(".scoreb");
-start.addEventListener("click",()=>{
-   
-    start.classList.toggle("started");
-    inp.style.display= "flex"
-    score.style.display= "flex"
-    scrollTo(score)
-    
+start.addEventListener("click", () => {
+  start.classList.toggle("started");
+  inp.style.display = "flex";
+  score.style.display = "flex";
+  scrollTo(score);
 
   setTimeout(() => {
-    
     fetch("city.json")
-  .then((response) => response.json())
-  .then((data) => {
-    cities.push(...data);
-
-    run();
-  });
+      .then((response) => response.json())
+      .then((data) => {
+        cities.push(...data);
+        run();
+      });
   }, 1000);
-
-})
-
+});
 
 function run() {
   const snum = Math.floor(Math.random() * 1223);
@@ -35,45 +29,38 @@ function run() {
   scity = cities[snum].name;
   document.querySelector(".citybox").innerHTML = `
       <h1 class="city"> ${scity} </h1>
-      `;
+      `;  
 }
-
 const checkAnswer = () => {
-  let aa = input.value.toLowerCase();
-  let b = cityState.toLowerCase();
+  let indata = input.value.toLowerCase();
+  let statedata = cityState.toLowerCase();
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 
-  if (aa === b) {
-    
+  if (indata === statedata) {
     count++;
     document.querySelector(".score").innerHTML = count;
     document.querySelector(".citybox").innerHTML = `
-      <h1 class="city"> Correct Answer </h1>
-      
-      `;
-    document.querySelector(".city").style.border = "4px solid green"
-    
+    <h1 class="city"> Correct Answer </h1>   
+    `;
+    document.querySelector(".city").style.border = "4px solid green";
     setTimeout(() => {
-      run()
+      run();
     }, 1500);
-    
     input.value = "";
   } else {
-    
-    
-    
-    document.querySelector(".score").innerHTML = count;
     document.querySelector(".citybox").innerHTML = `
     <h1 class="city"> Game Over!!! <br>
-      You Scored ${count} <br>
-      Auto Restart in 3 second 
-      </h1>
-      `;
-    document.querySelector(".city").style.border = "4px solid red"
+    You Scored ${count} <br>
+    Auto Restart in 3 second 
+    </h1>
+    `;
+    count = 0;
+    document.querySelector(".score").innerHTML = count;
+    document.querySelector(".city").style.border = "4px solid red";
     input.value = "";
     setTimeout(() => {
       run();
-      count = 0;
     }, 3000);
   }
-   
 };
